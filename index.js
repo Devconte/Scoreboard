80301aae03d2b6285aae27086734bdce373763d1
Devconte <romain.lcnte@gmail.com>
2023-09-13 15:44:18 +0200

    refactor : project cleaned

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-let homeStore = document.getElementById("home-score")
+let homeScoreEl = document.querySelector("#home-score");
+let guestScoreEl = document.querySelector("#guest-score");
+
 let homeScore = 0
+let guestScore = 0 
 
-function addOneHome() {
-    homeScore += 1
-    homeStore.textContent = homeScore
-}
-function addTwoHome() {
-    homeScore += 2
-    homeStore.textContent = homeScore
-}
-function addThreeHome() {
-    homeScore += 3
-    homeStore.textContent = homeScore
+function add(points, team) {
+  if(team === "home") {
+    homeScore += points
+    homeScoreEl.textContent = homeScore
+  } else {  
+    guestScore += points
+    guestScoreEl.textContent = guestScore
+  }
 }
 
-
-let guestStore = document.getElementById("guest-score")
-let guestScore = 0
-
-function addOneGuest() {
-    guestScore += 1
-    guestStore.textContent = guestScore
-}
-function addTwoGuest() {
-    guestScore += 2
-    guestStore.textContent = guestScore
-}
-function addThreeGuest() {
-    guestScore += 3
-    guestStore.textContent = guestScore
-}
-
-function newGame() {
-    guestStore.textContent = 0
-    homeStore.textContent = 0
-}
